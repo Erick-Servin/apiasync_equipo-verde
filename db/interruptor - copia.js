5ec42acf2d5974dd531361c97d42f29920a77db5
Erick-Servin <62585236+Erick-Servin@users.noreply.github.com>
2020-06-07 22:07:13 -0500

    rm inter

--- db/interruptor - copia.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const InterruptSchema = Schema({
-	Lado: String,
-	Fecha: new Date()
-});
-
-module.exports = mongoose.model('inter', InterruptSchema);
